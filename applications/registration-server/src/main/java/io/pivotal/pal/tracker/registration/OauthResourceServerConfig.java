0c583b67f8dd049384403a7f6e29047e9e42c43d
Sumit <sumit.mundada@cognizant.com>
2020-03-22 11:55:45 -0500

    restOperation method commented

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/OauthResourceServerConfig.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/OauthResourceServerConfig.java
@@ -16,11 +16,11 @@ import org.springframework.web.client.RestOperations;
 @ConditionalOnProperty(value = "application.oauth-enabled", matchIfMissing = true)
 public class OauthResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Bean
-    @LoadBalanced
-    public RestOperations restTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext oauth2ClientContext) {
-        return new OAuth2RestTemplate(resource, oauth2ClientContext);
-    }
+//    @Bean
+//    @LoadBalanced
+//    public RestOperations restTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext oauth2ClientContext) {
+//        return new OAuth2RestTemplate(resource, oauth2ClientContext);
+//    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
